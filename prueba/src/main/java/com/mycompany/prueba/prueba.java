8ab971b4aa63e0eca3938a64993759d5e3db2ce0
Programador02 <Programador02@192.168.2.3>
2013-03-27 17:01:10 -0600

    Esta es una modificaciopn de prueba

--- prueba/src/main/java/com/mycompany/prueba/prueba.java
+++ prueba/src/main/java/com/mycompany/prueba/prueba.java
@@ -13,6 +13,7 @@ public class prueba
     public static void main( String[] args )
     {
         System.out.println( "Este es el primer repositorio" );
+        System.out.print("Esta linea es una actualizacion para ver en github");
     }
     
 }
