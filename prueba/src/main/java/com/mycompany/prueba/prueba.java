8a9c470016f19458ddee12082f43a27b567f5ca4
Programador02 <Programador02@192.168.2.3>
2013-03-27 16:49:29 -0600

    esta es una prueba github al servidor github

---
+++ prueba/src/main/java/com/mycompany/prueba/prueba.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author Programador02
+ */
+public class prueba 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Este es el primer repositorio" );
+    }
+    
+}
