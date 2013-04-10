ec56e3c4c8fe454a74fcdce5d2e2d4d1a12e5c63
Programador02 <Programador02@192.168.2.3>
2013-04-10 17:33:21 -0500

    Este es el template

---
+++ prueba/src/main/java/com/mycompany/prueba/branch.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.prueba;
+
+/**
+ *
+ * @author Programador02
+ */
+public class branch {
+    public static void main( String[] args )
+    {
+        System.out.println( "Este es un Branch del repositorio prueba" );
+    }
+    
+}
